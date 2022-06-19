6b8f3a13cb8f23ddba207280f584da5426a20d25
nedbalski <nedbalski@gmail.com>
2022-06-19 18:27:31 +0300

    signature draft

--- app/signature.go
+++ app/signature.go
@@ -1,18 +1,52 @@
 package app
 
 import (
+	md52 "crypto/md5"
+	"encoding/binary"
+	"fmt"
 	"hash/adler32"
 	"io"
 )
 
+const maxBlockSize = 512
+
 type signature struct {
+	blockSize   int
+	rollingHash map[uint32]int64
+	md5Hash     [][16]byte
 }
 
-func NewSignature(_ io.Reader, _ io.Writer) (*signature, error) {
-	return &signature{}, nil
-}
+func NewSignature(in io.Reader, out io.Writer) (*signature, error) {
+	blockSize := maxBlockSize
+	sig := &signature{
+		blockSize:   blockSize,
+		rollingHash: map[uint32]int64{},
+		md5Hash:     make([][16]byte, 0),
+	}
+	for {
+		block := make([]byte, blockSize)
+		_, err := in.Read(block)
+		if err == io.ErrUnexpectedEOF || err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("smth went wrong, %w", err)
+		}
 
-func hash() {
-	a := adler32.New()
-	a.Sum32()
+		checkSum := adler32.Checksum(block)
+		md5 := md52.Sum(block)
+
+		err = binary.Write(out, binary.BigEndian, checkSum)
+		if err != nil {
+			return nil, fmt.Errorf("smth went wrong, %w", err)
+		}
+		err = binary.Write(out, binary.BigEndian, md5)
+		if err != nil {
+			return nil, fmt.Errorf("smth went wrong, %w", err)
+		}
+		sig.rollingHash[checkSum] = int64(len(sig.md5Hash))
+		sig.md5Hash = append(sig.md5Hash, md5)
+	}
+
+	return sig, nil
 }
